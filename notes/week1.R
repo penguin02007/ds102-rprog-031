e44c5f5130fe973c28d0702c17ed2f2d70da0f43
Leo Chan <penguin02007@gmail.com>
2015-08-16 19:14:45 -0400

    week1 syntax test

--- notes/week1.R
+++ notes/week1.R
@@ -21,7 +21,8 @@
 
 ## dget - use on single object, dump uses on multiple objects
 
-```Open a File Connection
+## Open a File Connection
+```{r}
 fh1 <- file("foo.txt", "r")
 data <- read.csv(fh1)
 close(fh1)
